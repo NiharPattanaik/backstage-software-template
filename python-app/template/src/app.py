cb7772ccc639fbb8d1a8bc22ba389d3a725b5a00
Nihar Pattanaik <nihar_pattanaik@Nihars-MacBook-Pro.local>
2025-05-06 23:16:39 +0530

    Adds python app software template

---
+++ python-app/template/src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify(
+        {
+            'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'hostname' : socket.gethostname(),
+            'message': 'You are doing great, human!!!, Wow its working',
+            'Deployed On': 'Kubernetes',
+            'app_name': '${{values.app_name}}',
+            'env': '${{values.app_env}}'
+
+        }
+    )
+
+
+@app.route('/api/v1/healthz')
+def healthz():
+    return jsonify(
+        {'status': 'UP'}
+    ), 200
+
+# main driver function
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
